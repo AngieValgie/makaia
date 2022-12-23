b6f57433f5fb7f1746cf7cd867557b8a18ed955d
Pangie <angierincon326@gmail.com>
2022-12-23 15:54:15 -0500

    workshop

---
+++ clases/Pokemon/main.js
@@ -0,0 +1,56 @@
+const CardsContainer = document.getElementById("CardsContainer");
+
+let habilidades = [];
+let nombre = "";
+let peso = "";
+let altura = "";
+let experiencia = "";
+let img = "";
+
+const renderPoke = () => {
+  CardsContainer.innerHTML = "";
+  CardsContainer.innerHTML += `
+   <figure class="CardPoke">
+        <img
+          src="${img}"
+          alt=""
+        />
+        <figcaption class="descripcionPokemon">
+          <h2>Nombre: ${nombre}</h2>
+          <h3>Peso: ${peso}</h3>
+          <h3>Altura: ${altura}</h3>
+          <ul>
+           <li>Habilidad: ${habilidades[0].ability.name}</li>
+           <li>habilidad: ${habilidades[1].ability.name}</li>
+          </ul>
+          <h3>Experiencia: ${experiencia}</h3>
+        </figcaption>
+      </figure>
+   `;
+};
+
+const InfoPoke = (data) => {
+  habilidades = data.abilities;
+  nombre = data.forms[0].name;
+  peso = data.weight;
+  altura = data.height;
+  experiencia = data.base_experience;
+  img = data.sprites.other["official-artwork"]["front_default"];
+  renderPoke();
+};
+
+//Hacemos un llamado a la api
+let url = "https://pokeapi.co/api/v2/pokemon/700/";
+fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => { //resultado de .JSON
+    InfoPoke(data);
+  })
+  .catch((error) => { //en caso de que haya error 
+    console.log("el error fue: " + error);
+  })
+  .finally(() => {
+    console.log("Termine");
+  });
---
+++ workshop/screen1/main.js
@@ -0,0 +1,50 @@
+const inputUno = document.getElementById('1')
+const inputDos = document.getElementById('2')
+const inputTres = document.getElementById('3')
+const inputCuatro = document.getElementById('4')
+const inputCinco = document.getElementById('5')
+const button = document.getElementById('button')
+const buttonsContainer = document.getElementById('buttonsContainer')
+const selected =  document.getElementById('selected')
+let numero;
+
+selected.style.display = 'none'
+
+button.addEventListener('click', calificacion)
+
+function calificacion(){
+    if(inputUno.checked){
+        numero = 1;
+    } else if (inputDos.checked){
+        numero = 2;
+    } else if (inputTres.checked){
+        numero = 3;
+    } else if (inputCuatro.checked){
+        numero = 4;
+    }else if(inputCinco.checked){
+        numero = 5;
+    }
+
+    localStorage.setItem('num', numero);
+
+    render()
+}
+
+const render = () => {
+  buttonsContainer.style.display = 'none'
+  selected.style.display = 'flex'
+  selected.innerHTML += `
+        <img src="/images/illustration-thank-you.svg" alt="">
+        <div class="selected">
+            <p>You selected ${localStorage.getItem('num')} out of 5</p>
+        </div>
+        <h1>Thank you!</h1>
+        <p>We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
+  ` 
+}
+
+const handleNum = () => {
+    console.log()
+}
+
+button.addEventListener('click', handleNum)
