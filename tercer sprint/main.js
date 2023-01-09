40eca99e20aa5cebe73dc54f87fb93bc32a21f52
Pangie <angierincon326@gmail.com>
2023-01-09 03:52:19 -0500

    arreglando kosas

--- tercer sprint/main.js
+++ tercer sprint/main.js
@@ -1,1 +1,8 @@
-console.log('hi')
+const modal = document.getElementById('modal');
+const close =  document.getElementById('close');
+
+modal.style.display = 'none';
+
+close.addEventListener('click', e => {
+    modal.style.display = 'none';
+})
