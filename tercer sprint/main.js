70cf8e2d9c1515d64884e8749a7e638fad10f8f2
Pangie <angierincon326@gmail.com>
2023-01-10 05:02:30 -0500

    mejorando el responsive

--- tercer sprint/main.js
+++ tercer sprint/main.js
@@ -1,7 +1,12 @@
 const modal = document.getElementById('modal');
 const close =  document.getElementById('close');
+const main__properties__Card = document.getElementById ('main__properties--Card');
 
 modal.style.display = 'none';
+
+main__properties__Card.addEventListener('click', e => {
+    modal.style.display = 'flex';
+})
 
 close.addEventListener('click', e => {
     modal.style.display = 'none';
