5671e5b3ed0a0ff703a597f8624c06f5c9f72443
Pangie <angierincon326@gmail.com>
2023-01-21 19:08:06 -0500

    idk

--- cuarto Sprint/js/main.js
+++ cuarto Sprint/js/main.js
@@ -1,43 +1,71 @@
 const API_ofertas = "http://localhost:3000/ofertas";
 const API_ubicaciones = "http://localhost:3000/ubicaciones";
-const API_usuarios = "http://localhost:3000/usuarios"
+const API_usuarios = "http://localhost:3000/usuarios";
 
-const close = document.getElementById('close');
-const address = document.getElementById('address');
-const location = document.getElementById('location');
-const offerCard = document.getElementById('offerCard')
-const locationSelect =  document.getElementById('locationSelect');
-const locationName = document.getElementById('locationName');
-const productsInCart = document.getElementById ('productsInCart');
+const address = document.getElementById("address");
+const carrito = document.getElementById("carrito");
+const location = document.getElementById("location");
+const close = document.getElementById("close");
+const closeProducts = document.getElementById("closeProducts");
+const cerrarDetallesProducto = document.getElementById(
+  "cerrarDetallesProducto"
+);
+const offerCard = document.getElementById("offerCard");
+const detallesProducto = document.getElementById("detallesProducto");
+const carrusel = document.getElementById("carrusel");
+const locationSelect = document.getElementById("locationSelect");
+const locationName = document.getElementById("locationName");
+const productsInCart = document.getElementById("productsInCart");
+const subtractProduct = document.getElementById("subtractProduct");
+const sumProduct = document.getElementById("sumProduct");
+const amount = document.querySelector(".amount");
+const addRemove = document.querySelector(".addRemove");
+const deliver = document.querySelector(".deliver");
+let cantidad = 0;
+let productos = [];
+let ubi = [];
 
-locationSelect.style.display = 'none';
+const productosAPI = (data) => {
+  productos = data;
+  console.log(productos);
+};
 
-locationName.addEventListener('click', e => {
-    locationSelect.style.display = 'flex';
-})
+const ubiAPI = (data) => {
+  ubi = data;
+  console.log(ubi);
+};
 
-close.addEventListener('click', e => {
-    locationSelect.style.display = 'none';
-})
+productsInCart.style.display = "none";
+carrito.addEventListener("click", (e) => {
+  productsInCart.style.display = "flex";
+});
 
-const opcionesUbicaciones = async () => {
-    const respuesta = await fetch(API_ubicaciones);
-    const data = await respuesta.json()
-    data.forEach(element => {
-        const{id, ciudad} = element;
-        address.innerHTML += `
-        <option value="${id}">${ciudad}</option>
-        `
-    });
-}
-document.addEventListener('DOMContentLoaded', opcionesUbicaciones)
+closeProducts.addEventListener("click", (e) => {
+  productsInCart.style.display = "none";
+});
 
+detallesProducto.style.display = "none";
+cerrarDetallesProducto.addEventListener("click", (e) => {
+  detallesProducto.style.display = "none";
+});
+
+locationSelect.style.display = "none";
+locationName.addEventListener("click", (e) => {
+  locationSelect.style.display = "flex";
+});
+
+close.addEventListener("click", (e) => {
+  locationSelect.style.display = "none";
+});
+
+/* agregar los productos al inicio */
 const pintarProductos = async () => {
-    const respuesta = await fetch(API_ofertas);
-    const data = await respuesta.json()
-    data.forEach(element => {
-        const{nombre, precioFinal, imagen} = element;
-        offerCard.innerHTML += `
+  const respuesta = await fetch(API_ofertas);
+  const data = await respuesta.json();
+  productosAPI(data);
+  data.forEach((element) => {
+    const { id, nombre, precioFinal, imagen } = element;
+    offerCard.innerHTML += `
             <div class="offerCardiv">
                 <label id="discount" for="">32% dto.</label>
                 <div id="offerCardinfo" class="offerCardinfo">
@@ -46,11 +74,55 @@ const pintarProductos = async () => {
                         <p class="precio">${precioFinal}/kg</p>
                     </div>
                     <p class="nombre">${nombre}</p>
-                    <button class="button" type="button">Agregar</button>
+                    <button id="${id}" class="button" type="button">Agregar</button>
                 </div>
             </div>
-        `
-    });
-}
+        `;
+  });
+};
+document.addEventListener("DOMContentLoaded", pintarProductos);
 
-document.addEventListener('DOMContentLoaded', pintarProductos)
+/* para agregar las opcines de ubicación desde la API */
+const opcionesUbicaciones = async () => {
+  const respuesta = await fetch(API_ubicaciones);
+  const data = await respuesta.json();
+  ubiAPI(data);
+  data.forEach((element) => {
+    const { id, ciudad } = element;
+    address.innerHTML += `
+        <option value="${id}">${ciudad}</option>
+        `;
+    localStorage.setItem("Ciudad", ciudad);
+  });
+};
+document.addEventListener("DOMContentLoaded", opcionesUbicaciones);
+
+/* usar la ubicación guardada */
+const ubiGuardada = () => {
+  deliver.innerHTML += `
+  <p>Entregar en: <span>${localStorage.getItem("Ciudad")}</span></p>
+  `;
+};
+document.addEventListener("DOMContentLoaded", ubiGuardada);
+
+offerCard.addEventListener("click", (e) => {
+  if (e.target.localName == "button") {
+    const arrayFilter = productos.filter((item) => item.id == e.target.id);
+    console.log(arrayFilter);
+
+    detallesProducto.style.display = "flex";
+    const img = document.querySelector(".producto");
+    const detallesPrecio = document.querySelector(".detallesPrecio");
+    const nombrecito = document.querySelector(".nombrecito");
+
+    arrayFilter.forEach((item) => {
+      img.src = item.imagen;
+      detallesPrecio.textContent = item.precioFinal;
+      nombrecito.textContent = item.nombre;
+      /* detallesProducto.querySelector(".producto").src = item.imagen; */
+    });
+  }
+  addRemove.addEventListener("click", (e) => {
+    console.log("escuchando para sumar o restar");
+  });
+});
